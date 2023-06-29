cee341898e8af128fdb974bc448d2db7e0768fca
Jesús Villamarín <jesus.villamarin@soytul.com>
2023-06-29 03:06:54 -0500

    feat: 🎸 Add vercel adapter to SSR for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import astroI18next from "astro-i18next";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image(), astroI18next(), react()],
-  output: 'hybrid'
+  output: 'hybrid',
+  adapter: vercel()
 });
